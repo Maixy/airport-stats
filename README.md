250c388f18a48057de576d47ead16a463954c29c
MikeC <N/A>
2023-07-03 13:57:19 -0700

    Basic script, initial GHA config

---
+++ README.md
@@ -0,0 +1,37 @@
+# Airport Stats Reporter
+### TL;DR - Queries for arrived and departed flights and reports data to Honeycomb
+
+This project is meant to populate a Honeycomb dataset with flight reports from 
+AeroAPI.  By sending up flattened wide flight event data we should be able to easily
+* **Watch trending flight performance over time.**  Ex. number of delayed flights, severity
+of delays, number of cancellations, etc. with a simple Honeycomb dashboard
+* **Alert on flight perf degradation** When delays and cancellations start piling up, this
+should be easily linkable to whatever webhooks we want via Honeycomb triggers. 
+* **Break down performance by {all the things}.** With wide data in place, we should be
+able to see performance breakdowns by Airline, originating (or destination) airport, 
+time of day, route, etc etc.  This part will be a fun pattern matching exercise. 
+
+## Arch & Costs
+Architecture here is real basic (and real free).
+
+#### Flight API in use is [FlightAware's AeroAPI](https://flightaware.com/commercial/aeroapi/).  
+Free tier supports up to 10 requests per minute.  We're running on a once-per-minute cron, so 
+nothing to worry about here. 
+
+#### Data storage and visualization is through [Honeycomb](https://www.honeycomb.io/).
+Free tier here supports 20 million events per month.  Initially running this for my 
+own use covering KSEA, which is doing ~35,000 flights per month.  Plenty of headroom.
+
+#### Execution is run via [Github Actions](https://github.com/features/actions)
+As a public repository, GH actions are free, so the scheduled cron (1x minute) will
+run at no cost on Github hosted runners. 
+
+
+## Accepted shortcomings
+This is mostly a POC + some fun trend data for my own use.  As such, it's not really that 
+accurate.  The most obvious failing here is that we're doing a lookback of 2m for flights 
+even though we're running every 1m.  This will help cover some weird time boundary and 
+downtime conditions (none of the above services offer an amazing availability SLA), in 
+exchange for some data duplication (up to 2x per flight).  We could be a bit smarter here 
+and just store the last successful execution time somewhere (maybe even in GHA), but 
+that's a project for another day.
---
+++ main.py
@@ -0,0 +1,84 @@
+import requests
+import os
+from datetime import datetime, timedelta
+import libhoney
+import time
+
+def timestamp_string(minutesAgo=0):
+    current_time = datetime.now()
+    ts = current_time - timedelta(minutes=minutesAgo)
+    formatted_ts = ts.strftime('%Y-%m-%dT%H:%M:%SZ')
+    return formatted_ts
+
+def flatten_json(json_obj, parent_key='', flattened_dict=None):
+    if flattened_dict is None:
+        flattened_dict = {}
+
+    if isinstance(json_obj, dict):
+        for key, value in json_obj.items():
+            new_key = f"{parent_key}.{key}" if parent_key else key
+            flatten_json(value, new_key, flattened_dict)
+    elif isinstance(json_obj, list):
+        for i, item in enumerate(json_obj):
+            new_key = f"{parent_key}.{i}" if parent_key else str(i)
+            flatten_json(item, new_key, flattened_dict)
+    else:
+        flattened_dict[parent_key] = json_obj
+
+    return flattened_dict
+
+def flightsFromResponse(payload): 
+    flights = []
+    arrivalsPayload = payload['arrivals']
+    for flight in arrivalsPayload:
+        flattened_arrival = flatten_json(flight)
+        flights.append(flattened_arrival)
+    
+    departuresPayload = payload['departures']
+    for flight in departuresPayload:
+        flattened_departure = flatten_json(flight)
+        flights.append(flight)
+
+    return flights
+
+def report_to_honeycomb(flights):
+    honeycomb_api_key = os.environ['HONEYCOMB_API_KEY']
+    libhoney.init(writekey=honeycomb_api_key, dataset="airport-stats-ksea", send_frequency=0, debug=True)
+    for flight in flights: 
+        print(flight)
+        event = libhoney.new_event()     
+        event.add(flight)
+        event.send()
+    libhoney.close()
+
+aeroapi_key = os.environ['AEROAPI_KEY']
+apiUrl = "https://aeroapi.flightaware.com/aeroapi/"
+
+fiveMinAgo = timestamp_string(5)
+now = timestamp_string()
+print('Retrieving all flights since {}'.format(fiveMinAgo))
+airport = 'KSEA'
+payload = {
+    'max_pages': 2,
+    'type': 'Airline',
+    'start': fiveMinAgo,
+    'end': now
+}
+
+print('Parameters: {}'.format(payload))
+auth_header = {'x-apikey':aeroapi_key}
+
+response = requests.get(apiUrl + f"airports/{airport}/flights",
+    params=payload, headers=auth_header)
+
+if response.status_code == 200:
+    jsonResponse = response.json()
+    flights = flightsFromResponse(jsonResponse)
+    print('{} flights received.'.format(len(flights)))
+    report_to_honeycomb(flights)
+    time.sleep(2) # sleep for a 2 seconds to give honeycomb push time to complete
+
+else:
+    print("Error executing request")
+    print(response.request.url)
+    print(response.request.body)
